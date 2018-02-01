63df7e248c75c441d253de25848d851e430ffb7d
Blake Redenius <blakeredenius@gmail.com>
2018-02-01 21:14:41 +1000

    Add POST support

--- src/main/java/me/blayyke/reflex/HttpClient.java
+++ src/main/java/me/blayyke/reflex/HttpClient.java
@@ -1,10 +1,10 @@
 package me.blayyke.reflex;
 
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
+import okhttp3.*;
+import org.json.JSONObject;
 
 public class HttpClient {
+    private final MediaType jsonMediaType = MediaType.parse("application/json; charset=utf-8");
     private final OkHttpClient client;
 
     HttpClient() {
@@ -13,5 +13,9 @@ public class HttpClient {
 
     public void get(Callback callback, String url) {
         client.newCall(new Request.Builder().url(url).build()).enqueue(callback);
+    }
+
+    public void post(Callback callback, String url, JSONObject body) {
+        client.newCall(new Request.Builder().post(RequestBody.create(jsonMediaType, body.toString())).url(url).build()).enqueue(callback);
     }
 }
