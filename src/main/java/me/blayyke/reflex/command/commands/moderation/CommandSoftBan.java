99d810adb93c484be31c2fe4321b2c0c75e1dcf8
Blake Redenius <blakeredenius@gmail.com>
2018-02-03 02:04:12 +1000

    Create softban command.

---
+++ src/main/java/me/blayyke/reflex/command/commands/moderation/CommandSoftBan.java
@@ -0,0 +1,41 @@
+package me.blayyke.reflex.command.commands.moderation;
+
+import me.blayyke.reflex.command.CommandPunishment;
+import me.blayyke.reflex.utils.UserUtils;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.Member;
+
+public class CommandSoftBan extends CommandPunishment {
+    @Override
+    public Permission[] getBotRequiredPermissions() {
+        return new Permission[]{Permission.BAN_MEMBERS};
+    }
+
+    @Override
+    public Permission[] getRequiredPermissions() {
+        return new Permission[]{Permission.BAN_MEMBERS};
+    }
+
+    @Override
+    public EmbedBuilder applyPunishment(EmbedBuilder embed, Member member, Member requester) {
+        member.getGuild().getController().ban(member, 7).reason("Soft-ban requested by " + UserUtils.formatUser(requester.getUser())).queue(aVoid ->
+                member.getGuild().getController().unban(member.getUser()).reason("Soft-ban requested by " + UserUtils.formatUser(requester.getUser())).queue()
+        );
+
+        embed.setTitle("Member soft-banned");
+        embed.setDescription("Successfully soft-banned " + UserUtils.formatUser(member.getUser()));
+
+        return embed;
+    }
+
+    @Override
+    public String getName() {
+        return "softban";
+    }
+
+    @Override
+    public String getDesc() {
+        return "Soft-ban a member from the server";
+    }
+}
