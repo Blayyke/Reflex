841f92725aafe1ffa8dfcbb0f900b4cd12e11d8e
Blake Redenius <blakeredenius@gmail.com>
2018-02-05 18:00:21 +1000

    Add HKey support

--- src/main/java/me/blayyke/reflex/database/DBEntryKey.java
+++ src/main/java/me/blayyke/reflex/database/DBEntryKey.java
@@ -1,22 +1,25 @@
 package me.blayyke.reflex.database;
 
 public enum DBEntryKey {
-    LEAVE_MESSAGE("leave_message", true, false, false),
-    JOIN_MESSAGE("join_message", true, false, false),
-    GUILD_PREFIX("prefix", true, false, false),
-    ANNOUNCEMENT_CHANNEL("announcement_channel", false, false, true),
-    AUTOROLE_ID("autorole_id", false, false, true);
+    LEAVE_MESSAGE("leave_message", true, false, false, false),
+    JOIN_MESSAGE("join_message", true, false, false, false),
+    GUILD_PREFIX("prefix", true, false, false, false),
+    ANNOUNCEMENT_CHANNEL("announcement_channel", false, false, true, false),
+    AUTOROLE_ID("autorole_id", false, false, true, false),
+    COMMANDS("commands", false, false, false, true);
 
     private final String redisKey;
     private final boolean isString;
     private final boolean isBool;
     private final boolean isNumber;
+    private boolean hash;
 
-    DBEntryKey(String redisKey, boolean isString, boolean isBool, boolean isNumber) {
+    DBEntryKey(String redisKey, boolean isString, boolean isBool, boolean isNumber, boolean hash) {
         this.redisKey = redisKey;
         this.isString = isString;
         this.isBool = isBool;
         this.isNumber = isNumber;
+        this.hash = hash;
     }
 
     public String getRedisKey() {
@@ -33,5 +36,9 @@ public enum DBEntryKey {
 
     public boolean isString() {
         return isString;
+    }
+
+    public boolean isHash() {
+        return hash;
     }
 }
