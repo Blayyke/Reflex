9630ffd437e42073e9465c56f1c9c385f697b494
Blake Redenius <blakeredenius@gmail.com>
2018-02-08 12:55:05 +1000

    Rename field `code` to `action`. Verify name is all letters.

--- src/main/java/me/blayyke/reflex/command/custom/CustomCommand.java
+++ src/main/java/me/blayyke/reflex/command/custom/CustomCommand.java
@@ -1,22 +1,30 @@
 package me.blayyke.reflex.command.custom;
 
+import me.blayyke.reflex.Reflex;
 import me.blayyke.reflex.command.AbstractCommand;
 import me.blayyke.reflex.command.CommandCategory;
 import me.blayyke.reflex.command.CommandContext;
+import me.blayyke.reflex.database.DBEntryKey;
+import me.blayyke.reflex.database.DBEntryKeyCCmd;
+import me.blayyke.reflex.utils.DatabaseUtils;
+import me.blayyke.reflex.utils.MiscUtils;
 import net.dv8tion.jda.core.entities.Guild;
 
 import javax.script.ScriptException;
 
 public class CustomCommand extends AbstractCommand {
     private Guild guild;
-    private String code;
+    private String action = null;
+    private String desc = null;
     private String name;
-    private String desc;
 
-    public CustomCommand(Guild guild, String name) {
+    public CustomCommand(Reflex reflex, Guild guild, String name) {
         if (guild == null || name == null || name.isEmpty()) throw new IllegalArgumentException("invalid arguments");
+        if (!MiscUtils.isAllLetters(name))
+            throw new IllegalArgumentException("name cannot contain non-alphabetic characters");
         this.guild = guild;
         this.name = name;
+        this.setReflex(reflex);
     }
 
     @Override
@@ -24,16 +32,12 @@ public class CustomCommand extends AbstractCommand {
         return CommandCategory.CUSTOM;
     }
 
-    private String getCommandCode() {
-        return code;
-    }
-
     @Override
     public void execute(CommandContext context) {
         try {
             getReflex().getCustomCommandManager().getExecutionEngine().eval(
                     "(function(){"
-                            + getCommandCode()
+                            + getAction()
                             + "})();");
         } catch (ScriptException e) {
             throw new RuntimeException(e);
@@ -54,15 +58,17 @@ public class CustomCommand extends AbstractCommand {
         return guild;
     }
 
-    public void setCode(String code) {
-        this.code = code;
+    public void setAction(String action) {
+        this.action = action;
+        DatabaseUtils.setHashString(getGuild(), getReflex().getDBManager().getSync(), DBEntryKey.CUSTOM_COMMAND.getRedisKey() + "_" + name, DBEntryKeyCCmd.ACTION, action);
     }
 
     public void setDesc(String desc) {
         this.desc = desc;
+        DatabaseUtils.setHashString(getGuild(), getReflex().getDBManager().getSync(), DBEntryKey.CUSTOM_COMMAND.getRedisKey() + "_" + name, DBEntryKeyCCmd.DESCRIPTION, desc);
     }
 
-    public void setName(String name) {
-        this.name = name;
+    private String getAction() {
+        return action;
     }
 }
