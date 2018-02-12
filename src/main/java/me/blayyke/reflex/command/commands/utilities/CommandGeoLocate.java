a7a3c711afee0043f8135f04865f2a3f4683e769
Blake Redenius <blakeredenius@gmail.com>
2018-02-12 21:37:48 +1000

    Create Geo-Locate command.

---
+++ src/main/java/me/blayyke/reflex/command/commands/utilities/CommandGeoLocate.java
@@ -0,0 +1,50 @@
+package me.blayyke.reflex.command.commands.utilities;
+
+import me.blayyke.reflex.command.AbstractCommand;
+import me.blayyke.reflex.command.CommandCategory;
+import me.blayyke.reflex.command.CommandContext;
+import me.blayyke.reflex.utils.AbstractCallback;
+import okhttp3.Response;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class CommandGeoLocate extends AbstractCommand {
+    @Override
+    public CommandCategory getCategory() {
+        return CommandCategory.UTILITIES;
+    }
+
+    @Override
+    public String getName() {
+        return "geoip";
+    }
+
+    @Override
+    public String[] getAliases() {
+        return new String[]{"geolocate"};
+    }
+
+    @Override
+    public String getDesc() {
+        return "Shows the rough geographical location of an IP address";
+    }
+
+    @Override
+    public void execute(CommandContext context) {
+        if (!context.hasArgs()) {
+            notEnoughArgs(context);
+            return;
+        }
+
+        String url = "https://freegeoip.net/json/" + context.getArgs()[0];
+        getReflex().getHttpClient().get(new AbstractCallback() {
+            @Override
+            public void response(Response response) throws IOException {
+                JSONObject bodyObj = new JSONObject(Objects.requireNonNull(response.body()).string());
+                context.getChannel().sendMessage(createEmbed().setTitle("IP Location").setDescription("The rough geographical location for that IP is " + bodyObj.getString("country_name") + "/" + bodyObj.getString("region_name") + "/" + bodyObj.getString("city")).build()).queue();
+            }
+        }, url);
+    }
+}
