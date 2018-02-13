d97378f3aead3c3aba095eda6ad452b5997550b3
Blake Redenius <blakeredenius@gmail.com>
2018-02-13 19:56:04 +1000

    Create invite command.

---
+++ src/main/java/me/blayyke/reflex/command/commands/utilities/CommandInvite.java
@@ -0,0 +1,33 @@
+package me.blayyke.reflex.command.commands.utilities;
+
+import me.blayyke.reflex.command.AbstractCommand;
+import me.blayyke.reflex.command.CommandCategory;
+import me.blayyke.reflex.command.CommandContext;
+
+public class CommandInvite extends AbstractCommand {
+    @Override
+    public CommandCategory getCategory() {
+        return CommandCategory.UTILITIES;
+    }
+
+    @Override
+    public String getName() {
+        return "invite";
+    }
+
+    @Override
+    public String[] getAliases() {
+        return new String[]{"guild", "support"};
+    }
+
+    @Override
+    public String getDesc() {
+        return "Send the invite URL for the support discord and the bot";
+    }
+
+    @Override
+    public void execute(CommandContext context) {
+        context.getChannel().sendMessage("**Support guild**: https://discord.gg/h5V3c9s\n" +
+                "**Bot invite**: https://reflex-bot.github.io/invite").queue();
+    }
+}
