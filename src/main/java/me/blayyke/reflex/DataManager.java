e0fdb3b74710186c5e7555fb55c0e3aa129e92ce
Blake Redenius <blakeredenius@gmail.com>
2018-02-15 06:45:09 +1000

    Create DataManager to remove some clutter from Reflex.

---
+++ src/main/java/me/blayyke/reflex/DataManager.java
@@ -0,0 +1,41 @@
+package me.blayyke.reflex;
+
+import me.blayyke.reflex.settings.BotSettings;
+import me.blayyke.reflex.utils.Version;
+import net.dv8tion.jda.core.entities.Guild;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataManager {
+    private final Reflex reflex;
+    private BotSettings settings;
+
+    private Map<Long, GuildStorage> guildStorageMap = new HashMap<>();
+
+    public DataManager(Reflex reflex) {
+        this.reflex = reflex;
+    }
+
+    public void init() throws IOException {
+        settings = new BotSettings(reflex);
+        settings.load();
+
+        Version.loadVersion();
+    }
+
+    public void setupGuildStorage(Guild guild) {
+        guildStorageMap.put(guild.getIdLong(), new GuildStorage(guild));
+    }
+
+    public GuildStorage getGuildStorage(Guild guild) {
+        if (!guildStorageMap.containsKey(guild.getIdLong())) setupGuildStorage(guild);
+
+        return guildStorageMap.get(guild.getIdLong());
+    }
+
+    public BotSettings getSettings() {
+        return settings;
+    }
+}
