af84b1f6b17633d8e809860042bf25b6ce7ee975
Blake Redenius <blakeredenius@gmail.com>
2018-02-16 19:15:16 +1000

    Create NSFW commands `r34` & `e621`.

---
+++ src/main/java/me/blayyke/reflex/command/commands/nsfw/CommandE621.java
@@ -0,0 +1,60 @@
+package me.blayyke.reflex.command.commands.nsfw;
+
+import me.blayyke.reflex.Colours;
+import me.blayyke.reflex.command.AbstractCommand;
+import me.blayyke.reflex.command.CommandCategory;
+import me.blayyke.reflex.command.CommandContext;
+import me.blayyke.reflex.utils.AbstractCallback;
+import me.blayyke.reflex.utils.MiscUtils;
+import net.dv8tion.jda.core.EmbedBuilder;
+import okhttp3.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class CommandE621 extends AbstractCommand {
+    @Override
+    public CommandCategory getCategory() {
+        return CommandCategory.NSFW;
+    }
+
+    @Override
+    public String getName() {
+        return "e621";
+    }
+
+    @Override
+    public String getDesc() {
+        return "Send an image from e621.net with your specified tags.";
+    }
+
+    @Override
+    public void execute(CommandContext context) {
+        String url = "https://e621.net/post/index.json?tags=" + MiscUtils.arrayToString(context.getArgs(), "_");
+        getReflex().getHttpClient().get(new AbstractCallback() {
+            @Override
+            public void response(Response response) throws IOException {
+                JSONArray posts = new JSONArray(Objects.requireNonNull(response.body()).string());
+                EmbedBuilder embedBuilder = createEmbed();
+                embedBuilder.setTitle("E621", "https://e621.net");
+
+                if (posts.length() < 1) {
+                    embedBuilder.setColor(Colours.WARN);
+                    embedBuilder.setDescription("No posts were found with your specified tags.");
+
+                    context.getChannel().sendMessage(embedBuilder.build()).queue();
+                    return;
+                }
+
+                JSONObject jsonObject = posts.getJSONObject(MiscUtils.getRandom(posts.length()));
+
+                embedBuilder.setImage(jsonObject.getString("file_url"));
+                embedBuilder.setDescription("Click [here](" + jsonObject.getString("source") + ") for source");
+
+                context.getChannel().sendMessage(embedBuilder.build()).queue();
+            }
+        }, url);
+    }
+}
