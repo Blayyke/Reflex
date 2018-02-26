bcc105e05e5d4d549c0edc430c0ab976a922b31e
Blake Redenius <blakeredenius@gmail.com>
2018-02-27 06:12:57 +1000

    Create `ImageCommand` class to much simplify basic commands that get an image from a website and send it.

---
+++ src/main/java/me/blayyke/reflex/command/ImageCommand.java
@@ -0,0 +1,47 @@
+package me.blayyke.reflex.command;
+
+import me.blayyke.reflex.utils.AbstractCallback;
+import me.blayyke.reflex.utils.MiscUtils;
+import net.dv8tion.jda.core.EmbedBuilder;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public abstract class ImageCommand extends AbstractCommand {
+    public abstract String getAPIUrl();
+
+    public abstract String getSiteUrl();
+
+    public abstract String handle(String string);
+
+    private interface Test {
+        void receive(String receive);
+    }
+
+    @Override
+    public final String getDesc() {
+        return "Get a random image from " + getSiteUrl();
+    }
+
+    @Override
+    public final void execute(CommandContext context) {
+        EmbedBuilder embedBuilder = createEmbed();
+
+        embedBuilder.setTitle(MiscUtils.uppercaseFirst(getName()), getSiteUrl());
+        get(receive -> {
+            embedBuilder.setImage(receive);
+            context.getChannel().sendMessage(embedBuilder.build()).queue();
+        });
+    }
+
+    private void get(Test test) {
+        getReflex().getHttpClient().get(new AbstractCallback() {
+            @Override
+            public void response(Response response) throws IOException {
+                test.receive(handle(Objects.requireNonNull(response.body()).string()));
+                response.close();
+            }
+        }, getAPIUrl());
+    }
+}
